8b1d9949b873e7f3fb0f7dd77d2457a424527e61
Fikayo Banjo <banjofikayo@gmail.com>
2024-11-09 15:41:55 +0100

    enhanced

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -6,8 +6,8 @@ function Counter() {
   return (
     <>
       <p>Count: {count}</p>
-      <button onClick={() => setCount((count) => count + 1)}> Increase</button>
-      <button onClick={() => setCount((count) => count - 1)}> Decrease</button>
+      <button onClick={() => setCount((count) => count + 1)}> Increment</button>
+      <button onClick={() => setCount((count) => count - 1)}> Decrement</button>
       <button onClick={() => setCount(0)}> Reset</button>
     </>
   );
