3ebd68970ccd465ba8ff7e6758d15abba2117b55
Fikayo Banjo <banjofikayo@gmail.com>
2024-11-09 15:40:29 +0100

    enhanced

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,10 +4,12 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <>
+      <Counter />
       <WelcomeMessage />
       <Header />
       <MainContent />
