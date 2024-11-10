8c1f9170bbc0cdab2960f8198ca9e8e5966fffa7
Fikayo Banjo <banjofikayo@gmail.com>
2024-11-10 13:40:34 +0100

    react

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
-import ProfilePage from "./ProfilePage";
-import UserContext from "./UserContext";
+import ProfilePage from "./components/ProfilePage";
+import UserContext from "./components/UserContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
