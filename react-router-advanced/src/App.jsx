c7524d62a9d4fcda98cdc0f1b3c1a7ea951aaf4f
Fikayo Banjo <banjofikayo@gmail.com>
2024-11-24 18:56:25 +0100

    Route

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,27 +1,47 @@
+// // src/App.jsx
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./components/Home";
+// import Profile from "./components/Profile";
+// import Login from "./components/Login";
+// import ProtectedRoute from "./components/ProtectedRoute";
+// const App = () => {
+//   const isAuthenticated = false;
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+
+//         <Route
+//           path="/profile/*"
+//           element={
+//             <ProtectedRoute isAuthenticated={isAuthenticated}>
+//               <Profile />
+//             </ProtectedRoute>
+//           }
+//         />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/blog/:postId" element={<BlogPost />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
-import Login from "./components/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
+import BlogPost from "./components/BlogPost";
+
 const App = () => {
-  const isAuthenticated = false;
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* Dynamic route */}
       </Routes>
     </Router>
   );
