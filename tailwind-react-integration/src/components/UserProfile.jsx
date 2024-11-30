5f0fca7106cb8f7debdee6a99b71a56f16e17a1f
Fikayo Banjo <banjofikayo@gmail.com>
2024-11-30 14:51:53 +0100

    tailwindcss

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 function UserProfile() {
   return (
-    <div className="user-profile sm:p-4 md:p-8 bg-gray-100  md:max-w-sm max-w-s max-w-xs mx-auto my-20 rounded-lg shadow-lg">
+    <div className="hover:text-blue-500 user-profile sm:p-4 md:p-8 bg-gray-100  md:max-w-sm max-w-s max-w-xs mx-auto my-20 rounded-lg shadow-lg">
       <img
-        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
+        className=" hover:scale-110 transition-transform duration-300 ease-in-out rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="sm: text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className=" hover:text-blue-500 sm: text-lg md:text-xl text-blue-800 my-4">
+        John Doe
+      </h1>
       <p className="sm:text-sm md:text-base text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
