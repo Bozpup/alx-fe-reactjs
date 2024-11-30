cc4e82a994ec2c772426b4fa8d36bfb44076cba9
Fikayo Banjo <banjofikayo@gmail.com>
2024-11-30 14:48:09 +0100

    tailwindcss

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="sm: text-lg md: text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className="sm: text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
       <p className="sm:text-sm md:text-base text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
