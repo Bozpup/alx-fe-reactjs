960de2f3c6a22292a335aa4c25020d22ef303182
Fikayo Banjo <banjofikayo@gmail.com>
2024-11-30 14:44:55 +0100

    tailwindcss

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 function UserProfile() {
   return (
-    <div className="user-profile sm: p-4 md:p-8 bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile sm:p-4 md:p-8 bg-gray-100  max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
-        className="rounded-full w-36 h-36 mx-auto sm:w-24 h-24 md:w-36 h-36"
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
         src="https://via.placeholder.com/150"
         alt="User"
       />
