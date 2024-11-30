accb53068147e838c4f2cfcbbb07543687dae0d6
Fikayo Banjo <banjofikayo@gmail.com>
2024-11-30 14:17:21 +0100

    tailwindcss

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <>
       <div className="bg-purple-300"> Hello</div>
+      <UserProfile />
     </>
   );
 }
