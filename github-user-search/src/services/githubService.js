166657772d709331b22c26c0e0ff90b18510dd75
Fikayo Banjo <banjofikayo@gmail.com>
2024-12-12 03:05:15 +0100

    search

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,15 +1,15 @@
 export const fetchUserData = async (username, location, minRepos) => {
-  let query = `q=${username}`; // Base query
+  let q = `q=${username}`; // Base q
 
   if (location) {
-    query += `+location:${location}`;
+    q += `+location:${location}`;
   }
 
   if (minRepos) {
-    query += `+repo:${minRepos}`;
+    q += `+repo:${minRepos}`;
   }
 
-  const url = `https://api.github.com/search/users?${query}`; // Construct URL
+  const url = `https://api.github.com/search/users?${q}`; // Construct URL
 
   try {
     const response = await axios.get(url);
